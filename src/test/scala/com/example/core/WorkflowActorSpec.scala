13683e555ab36a3a0272912a6700cebf7e034527
kartavtcev <kartavtcev.sergei@gmail.com>
2018-06-13 08:16:48 +0300

    Replaced auth, table manager actors with TestProbe

--- src/test/scala/com/example/core/WorkflowActorSpec.scala
+++ src/test/scala/com/example/core/WorkflowActorSpec.scala
@@ -2,9 +2,10 @@ package com.example.core
 
 import akka.actor.ActorSystem
 import akka.event.Logging
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.example.shared.PublicProtocol
 import org.scalatest.{BeforeAndAfterAll, MustMatchers, WordSpecLike}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -20,10 +21,13 @@ class WorkflowActorSpec(_system: ActorSystem) extends TestKit(_system) with Word
     "Process Join. Reply pong to ping message." in {
 
       lazy val log = Logging(system, classOf[WorkflowActorSpec])
-      val authActor = system.actorOf(AuthActor.props(log), "authActor")
-      val tableManagerActor = system.actorOf(TableManagerActor.props(log), "tableManagerActor")
+      val authActor = TestProbe()
+      val tableManagerActor = TestProbe()
 
-      val workflowActor = system.actorOf(WorkflowActor.props(log, authActor, tableManagerActor), "workflowActor")
+      //val authActor = system.actorOf(AuthActor.props(log), "authActor")
+      //val tableManagerActor = system.actorOf(TableManagerActor.props(log), "tableManagerActor")
+
+      val workflowActor = system.actorOf(WorkflowActor.props(log, authActor.ref, tableManagerActor.ref), "workflowActor")
 
       workflowActor ! PrivateProtocol.Joined("connect id UUID", testActor)
       workflowActor ! PrivateProtocol.IdWithInMessage("connect id UUID", PublicProtocol.ping(1))
